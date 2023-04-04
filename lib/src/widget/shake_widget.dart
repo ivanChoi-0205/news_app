4beaf7e94bf794d8fa8bd93cca2358f47b65fa46
Chun Hung, Choi <82143581+ivanChoi-0205@users.noreply.github.com>
2023-04-04 18:33:33 +0800

    feat: shade widget

---
+++ lib/src/widget/shake_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ShakeWidget extends StatelessWidget {
+  const ShakeWidget({
+    Key? key,
+    this.duration = const Duration(milliseconds: 500),
+    this.deltaX = 20,
+    this.curve = Curves.bounceOut,
+    required this.child,
+  }) : super(key: key);
+
+  final Duration duration;
+  final double deltaX;
+  final Widget child;
+  final Curve curve;
+
+  /// convert 0-1 to 0-1-0
+  double shake(double animation) =>
+      2 * (0.5 - (0.5 - curve.transform(animation)).abs());
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      key: key,
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: duration,
+      builder: (context, animation, child) => Transform.translate(
+        offset: Offset(deltaX * shake(animation), 0),
+        child: child,
+      ),
+      child: child,
+    );
+  }
+}
