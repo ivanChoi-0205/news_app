c6801dfec6a644bce7023416af56e3dcee642ee9
Chun Hung, Choi <82143581+ivanChoi-0205@users.noreply.github.com>
2023-04-25 00:43:30 +0800

    feat. add app bar background to news layout

---
+++ lib/src/feature/feature_main/news/widget/news_app_bar_background.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class NewsAppBarBackground extends StatelessWidget {
+  const NewsAppBarBackground({
+    Key? key,
+    required this.scrollRatio,
+  }) : super(key: key);
+
+  final double scrollRatio;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: _BackgroundClipper(
+        bottomShapeHeight: 35,
+        scrollRatio: scrollRatio,
+      ),
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
+
+class _BackgroundClipper extends CustomClipper<Path> {
+  _BackgroundClipper({
+    required this.bottomShapeHeight,
+    required this.scrollRatio,
+  });
+
+  final double bottomShapeHeight;
+  final double scrollRatio;
+
+  @override
+  Path getClip(Size size) {
+    final height = size.height;
+    final width = size.width;
+
+    // // method 1
+    // return Path()
+    //   ..lineTo(0, height)
+    //   ..quadraticBezierTo(
+    //     size.width / 2,
+    //     size.height - bottomShapeHeight,
+    //     size.width,
+    //     size.height,
+    //   )
+    //   ..lineTo(width, 0)
+    //   ..close();
+
+    // method 2
+    final ratio = bottomShapeHeight * (1 - scrollRatio);
+    return Path()
+      ..lineTo(0, height - ratio)
+      ..quadraticBezierTo(
+        width / 2,
+        height + ratio,
+        width,
+        height - ratio,
+      )
+      ..lineTo(width, 0)
+      ..close();
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    /// enable to trigger rebuild
+    return false;
+    // return true;
+  }
+}
---
+++ lib/src/feature/feature_main/news/widget/news_app_bar_leading.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../../constant/global_key.dart';
+import '../../../../service/vibration_service.dart';
+
+class NewsAppBarLeading extends StatelessWidget {
+  const NewsAppBarLeading({
+    Key? key,
+    required this.scrollRatio,
+  }) : super(key: key);
+
+  final double scrollRatio;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: () {
+              clickFeedback();
+              rootScaffoldKey.currentState!.openDrawer();
+            },
+            child: const Icon(
+              Icons.menu_rounded,
+              color: Colors.black,
+            ),
+          ),
+          const Spacer(),
+          const CircleAvatar(
+            child: Icon(
+              Icons.notifications,
+            ),
+          ),
+          const SizedBox(width: 20),
+          const CircleAvatar(
+            child: Icon(
+              Icons.person_outline_rounded,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
